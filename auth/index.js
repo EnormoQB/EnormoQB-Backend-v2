c600226700a7c9baf82c832256d95ef9638ed27b
ankitgoel25 <ankitoct01@gmail.com>
2022-08-02 16:07:38 +0530

    Fixed routing for production

--- auth/index.js
+++ auth/index.js
@@ -11,7 +11,7 @@ passport.use(
     {
       clientID: `${process.env.GOOGLE_CLIENT_ID}`,
       clientSecret: `${process.env.GOOGLE_CLIENT_SECRET}`,
-      callbackURL: '/auth/google/callback',
+      callbackURL: `${process.env.BASE_PATH}auth/google/callback`,
     },
     (_, __, profile, cb) => {
       try {
