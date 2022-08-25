528d46abfea6b60b30cf52c7b5b244e985c4bd80
Apoorv Dwivedi <apoorvd14@gmail.com>
2022-08-25 11:47:18 +0530

    added sendgrid for email

--- helpers/mailUtil.js
+++ helpers/mailUtil.js
@@ -1,23 +1,18 @@
-const nodemailer = require('nodemailer');
-const logger = require('./winston');
 require('dotenv').config();
+const sgMail = require('@sendgrid/mail');
+const logger = require('./winston');
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const main = async (mailOptions) => {
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      type: 'OAuth2',
-      user: process.env.ADMIN_EMAIL,
-      pass: process.env.SMTP_PASSWORD,
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
-    },
-  });
-
-  const info = await transporter.sendMail(mailOptions);
-
-  logger.info('Email sent successfully', info.messageId);
+  await sgMail
+    .sendMultiple(mailOptions)
+    .then((response) => {
+      logger.info('Email sent successfully', response);
+    })
+    .catch((error) => {
+      logger.error('Error', error);
+    });
 };
 
 module.exports = main;
