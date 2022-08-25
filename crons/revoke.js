75a28eeda7ffb3a1c3fe93274711b995bab005c6
ankitgoel25 <ankitoct01@gmail.com>
2022-08-26 00:38:12 +0530

    Exam setter access revoke cron job added

--- crons/revoke.js
+++ crons/revoke.js
@@ -1,9 +1,17 @@
 const cron = require('node-cron');
 const User = require('../models/UserModel');
 
-// every minute
-cron.schedule('* * * * * *', async () => {
-  const users = await User.find({});
+// runs everyday at 12:00 AM
+cron.schedule('0 0 0 * * *', async () => {
+  const users = (await User.find()).filter((user) => {
+    if (user.userType !== 'exam-setter') return false;
+    return new Date() - new Date(user.typeLastChanged) >= 3.6e3 * 48; // greater than 48 hrs
+  });
 
-  //   users.map((user) => {});
+  users.forEach((user) => {
+    User.findByIdAndUpdate(user._id, {
+      typeLastChanged: new Date(),
+      userType: 'reviewer',
+    }).exec();
+  });
 });
